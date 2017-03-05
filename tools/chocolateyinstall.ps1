bc4e2a114d3182c5dc1cb07c7a96a3cdd56b0b24
Junichi Kato <j5ik2o@gmail.com>
2017-03-06 03:25:06 +0900

    added files

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'scala.install'
+$installerType = 'msi'
+$url = 'http://www.scala-lang.org/files/archive/scala-2.12.1.msi'
+$silentArgs = '/qn /norestart'
+ 
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url
